3dc72abddb6c9afe885a29160b65c5e2b8441e7f
k944 <17305525739@163.com>
2020-10-10 20:47:29 +0800

    修改了

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="app-content">
+  <mt-header fixed title="黑马购物"></mt-header>
+    <h1>这是 App 组件</h1>
+
+
+
+
+
+
+
+    <nav class="mui-bar mui-bar-tab">
+<a class="mui-tab-item mui-active" href="#tabber">
+<span class="mui-icon mui-icon-home"></span>
+<span class="mui-tab-label">首页</span>
+</a>
+
+<a class="mui-tab-item " href="#tabber-with-chat">
+<span class="mui-icon mui-icon-email">
+<span class="badge">9</span>
+</span>
+
+<span class="mui-tab-label">消息</span>
+
+</a>
+<a class="mui-tab-item mui-active" href="#tabber-with-contact">
+<span class="mui-icon mui-icon-contact"></span>
+<span class="mui-tab-label">通讯录</span>
+</a>
+<a class="mui-tab-item mui-active" href="#tabber-with-map">
+<span class="mui-icon mui-icon-gear"></span>
+<span class="mui-tab-label">设置</span>
+</a>
+
+    </nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style lang="scss" scoped>
+.app-content{
+  padding-top:40px;
+}
+</style>
